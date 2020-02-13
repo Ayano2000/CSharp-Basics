2385284ad556ef96562c8672c4963ba8c8d19c04
Arata Yano <aratayano00@gmail.com>
2020-02-13 11:08:41 +0200

    i tihnk i lost some work

---
+++ NumberGuesser/testApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// name space is similar to package in java.
+namespace testApp
+{
+    // this is our main class.
+    class Program
+    {
+        // this is our main method(entry point).
+        static void Main(string[] args)
+        {
+            // set app variables
+            string appName = "testApp";
+            string appVersion = "1.0.0";
+            string appAuthor = "Arata Yano";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+
+            Console.WriteLine("Please enter your username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Welcome {0}", username);
+        }
+    }
+}
